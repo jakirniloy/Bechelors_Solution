044b222a63ef05e8189c733ef9930a7a875e0a2a
Jakir Hossain Niloy <mdjakirhossen13@gmail.com>
2022-04-16 23:35:59 +0600

    signup/login

---
+++ app/src/main/java/me/jakirniloy/bechelors_solution/signup.java
@@ -0,0 +1,28 @@
+package me.jakirniloy.bechelors_solution;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        TextView alreadySiginup = findViewById(R.id.registerAlreadyAccountTextID);
+        alreadySiginup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(signup.this,login  .class));
+            }
+        });
+
+    }
+
+
+}
