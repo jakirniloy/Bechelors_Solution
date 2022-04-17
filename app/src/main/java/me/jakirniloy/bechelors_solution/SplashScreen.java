0977bf99e91e1349f7942fee000e60db2f7dd5b0
rifat0711 <57683016+rifat0711@users.noreply.github.com>
2022-04-18 01:46:07 +0600

    SplashScreen

---
+++ app/src/main/java/me/jakirniloy/bechelors_solution/SplashScreen.java
@@ -0,0 +1,48 @@
+package me.jakirniloy.bechelors_solution;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ProgressBar;
+
+public class SplashScreen extends AppCompatActivity {
+private ProgressBar Progressbar;
+private int progress;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        Progressbar =findViewById(R.id.progressbar);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                dowork();
+                Startapp();
+            }
+        });
+        thread.start();
+    }
+    public void dowork(){
+        for(progress=20;progress<=100;progress= progress+20){
+            try {
+                Thread.sleep(1000);
+                Progressbar.setProgress(progress);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+    }
+    public void Startapp(){
+        Intent intent = new Intent(SplashScreen.this,login.class);
+        startActivity(intent);
+        finish();
+    }
+}
